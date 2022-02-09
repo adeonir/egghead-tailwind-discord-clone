16c06f0ebfc74855ff51e613b3f266d400a7ab67
Adeonir Kohl <adeonir@gmail.com>
2022-02-09 17:37:14 -0300

    Add custom scrollbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen text-gray-100">
-      <div className="space-y-3 overflow-y-auto bg-gray-900 p-3">
+      <div className="p-r-6 space-y-3 overflow-y-auto bg-gray-900 p-3 scrollbar scrollbar-none">
         {[...Array(20)].map((_, index) => (
           <div
             key={index}
@@ -12,10 +12,10 @@ export default function Home() {
         ))}
       </div>
       <div className="flex w-60 flex-col bg-gray-800">
-        <div className="flex h-12 items-center px-3 font-title text-white shadow-md">
+        <div className="flex h-12 items-center border-b border-gray-800 px-3 font-title text-white shadow-md">
           Tailwind CSS
         </div>
-        <div className="flex-1 space-y-3 overflow-y-auto p-3 font-medium text-gray-300">
+        <div className="flex-1 space-y-3 overflow-y-auto p-3 font-medium text-gray-300 scrollbar scrollbar-thin scrollbar-thumb-gray-900">
           <p className="text-white">Channel (unread)</p>
           {[...Array(40)].map((_, index) => (
             <p key={index}>Channel {index + 1}</p>
@@ -23,8 +23,10 @@ export default function Home() {
         </div>
       </div>
       <div className="flex flex-1 flex-col bg-gray-700">
-        <div className="flex h-12 items-center px-3 shadow-md">General</div>
-        <div className="flex-1 space-y-4 overflow-y-auto p-3">
+        <div className="flex h-12 items-center border-b border-gray-800 px-3 shadow-md">
+          General
+        </div>
+        <div className="flex-1 space-y-4 overflow-y-auto p-3 scrollbar scrollbar-thin scrollbar-track-gray-800 scrollbar-thumb-gray-900">
           {[...Array(40)].map((_, index) => (
             <div key={index}>
               Message {index + 1} Lorem, ipsum dolor sit amet consectetur
