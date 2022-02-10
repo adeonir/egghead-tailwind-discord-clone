256772fd3dfd3b1182b4727d0c5e0fdffecbd26c
Adeonir Kohl <adeonir@gmail.com>
2022-02-10 15:15:45 -0300

    Update nav link and channel link components

--- src/components/ChannelLink/ChannelLink.tsx
+++ src/components/ChannelLink/ChannelLink.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 import * as Icons from 'components/Icons'
 
 type Props = {
@@ -9,18 +12,27 @@ type Props = {
 }
 
 export const ChannelLink = ({ channel }: Props) => {
+  const router = useRouter()
+  const { sid, cid } = router.query
+  const active = Number(channel.id) === Number(cid)
+
   const Icon = channel.icon
     ? Icons[channel.icon as keyof typeof Icons]
     : Icons.Hashtag
 
   return (
-    <a
-      href="#"
-      className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 transition hover:bg-gray-550/[.16] hover:text-gray-100"
-    >
-      <Icon className="mr-1.5 h-5 w-5 pt-px text-gray-400" />
-      {channel.label}
-      <Icons.AddPerson className="ml-auto h-4 w-4 text-gray-200 opacity-0 transition group-hover:opacity-100 hover:text-gray-100" />
-    </a>
+    <Link href={`/servers/${sid}/channels/${channel.id}`}>
+      <a
+        className={`${
+          active
+            ? 'bg-gray-550/[0.32] text-white'
+            : 'text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100'
+        } group mx-2 flex items-center rounded px-2 py-1`}
+      >
+        <Icon className="mr-1.5 h-5 w-5 pt-px text-gray-400" />
+        {channel.label}
+        <Icons.AddPerson className="ml-auto h-4 w-4 text-gray-200 opacity-0 transition group-hover:opacity-100 hover:text-gray-100" />
+      </a>
+    </Link>
   )
 }
--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -5,10 +5,13 @@ import { ReactNode } from 'react'
 type Props = {
   children: ReactNode
   href: string
+  active?: boolean
 }
 
-export const NavLink = ({ children, href }: Props) => {
+export const NavLink = ({ children, href, active }: Props) => {
   const router = useRouter()
+
+  active ||= router.asPath === href
 
   return (
     <Link href={href}>
@@ -16,7 +19,7 @@ export const NavLink = ({ children, href }: Props) => {
         <div className="absolute -left-3 flex h-full items-center">
           <div
             className={`${
-              router.asPath === href
+              active
                 ? 'h-10'
                 : 'h-5 scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100'
             } w-1 origin-left rounded-r bg-white transition-all duration-300`}
@@ -26,7 +29,7 @@ export const NavLink = ({ children, href }: Props) => {
         <div className="group-active:translate-y-px">
           <div
             className={`${
-              router.asPath === href
+              active
                 ? 'cursor-default rounded-2xl bg-brand text-white'
                 : 'rounded-3xl bg-gray-700 font-medium text-gray-100 group-hover:rounded-2xl group-hover:bg-brand group-hover:text-white '
             } flex h-12 w-12 items-center justify-center overflow-hidden transition-all duration-300`}
