1d9fdff967e48bd1d86613ad8a3cfd40f363a514
Adeonir Kohl <adeonir@gmail.com>
2022-02-10 14:07:44 -0300

    Add types to server page

--- src/pages/servers/[slug].tsx
+++ src/pages/servers/[slug].tsx
@@ -1,13 +1,34 @@
 import { useRouter } from 'next/router'
-import startCase from 'lodash.startcase'
 
 import * as Icons from 'components/Icons'
 import { ChannelLink } from 'components/ChannelLink'
 
-import data from '../../../data.json'
+import { GetServerSideProps } from 'next'
 
-export default function Server() {
+type Data = {
+  data: {
+    [key: string]: {
+      label: string
+      categories: Category[]
+    }
+  }
+}
+
+type Category = {
+  id: string
+  label: string
+  channels: Channel[]
+}
+
+type Channel = {
+  id: number
+  label: string
+  icon?: string
+}
+
+export default function Server({ data }: Data) {
   const router = useRouter()
+  const { slug } = router.query
 
   return (
     <>
@@ -17,26 +38,28 @@ export default function Server() {
             <Icons.Verified className="absolute h-4 w-4 text-gray-550" />
             <Icons.Check className="absolute h-4 w-4 text-white" />
           </div>
-          {startCase(router.query.slug)}
+          {data[slug as keyof typeof data].label}
           <Icons.Chevron className="ml-auto h-[18px] w-[18px] opacity-80" />
         </button>
         <div className="flex-1 space-y-[21px] overflow-y-auto pt-3 font-medium text-gray-300 scrollbar scrollbar-thin scrollbar-thumb-gray-900">
-          {data[router.query.slug].categories.map((category) => (
-            <div key={category.id}>
-              {category.label && (
-                <button className="flex items-center px-0.5 font-title text-xs uppercase tracking-wide">
-                  <Icons.Arrow className="mr-0.5 h-3 w-3" />
-                  {category.label}
-                </button>
-              )}
+          {data[slug as keyof typeof data].categories.map(
+            (category: Category) => (
+              <div key={category.id}>
+                {category.label && (
+                  <button className="flex items-center px-0.5 font-title text-xs uppercase tracking-wide">
+                    <Icons.Arrow className="mr-0.5 h-3 w-3" />
+                    {category.label}
+                  </button>
+                )}
 
-              <div className="mt-[5px] space-y-0.5">
-                {category.channels.map((channel) => (
-                  <ChannelLink channel={channel} key={channel.id} />
-                ))}
+                <div className="mt-[5px] space-y-0.5">
+                  {category.channels.map((channel: Channel) => (
+                    <ChannelLink channel={channel} key={channel.id} />
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
       <div className="flex flex-1 flex-col bg-gray-700">
@@ -55,4 +78,14 @@ export default function Server() {
       </div>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = require('../../../data.json') as Data
+
+  return {
+    props: {
+      data,
+    },
+  }
 }
