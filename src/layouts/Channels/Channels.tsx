93f6eaf760a9df57414baad40051812c751432ce
Adeonir Kohl <adeonir@gmail.com>
2022-02-10 17:33:55 -0300

    Add header to messages section

--- src/layouts/Channels/Channels.tsx
+++ src/layouts/Channels/Channels.tsx
@@ -30,7 +30,11 @@ export type Channel = {
 
 export const Channels = ({ data }: Data) => {
   const router = useRouter()
-  const { sid } = router.query
+  const server = data[`${router.query.sid}`]
+  const channel = server.categories
+    .map((c) => c.channels)
+    .flat()
+    .find((c) => Number(c.id) === Number(router.query.cid))
 
   const [closedCategories, setClosedCategories] = useState<string[]>([])
 
@@ -50,47 +54,80 @@ export const Channels = ({ data }: Data) => {
             <Icons.Verified className="absolute h-4 w-4 text-gray-550" />
             <Icons.Check className="absolute h-4 w-4 text-white" />
           </div>
-          {data[sid as keyof typeof data].label}
+          {server.label}
           <Icons.Chevron className="ml-auto h-[18px] w-[18px] opacity-80" />
         </button>
-        <div className="flex-1 space-y-[21px] overflow-y-auto pt-3 font-medium text-gray-300 scrollbar scrollbar-thin scrollbar-thumb-gray-900">
-          {data[sid as keyof typeof data].categories.map(
-            (category: Category) => (
-              <div key={category.id}>
-                {category.label && (
-                  <button
-                    onClick={() => toggleCategory(category.id)}
-                    className="flex w-full items-center px-0.5 font-title text-xs uppercase tracking-wide hover:text-gray-100"
-                  >
-                    <Icons.Arrow
-                      className={`${
-                        closedCategories.includes(category.id)
-                          ? '-rotate-90'
-                          : ''
-                      } mr-0.5 h-3 w-3 transition duration-200`}
-                    />
-                    {category.label}
-                  </button>
-                )}
 
-                <div className="mt-[5px] space-y-0.5">
-                  {category.channels
-                    .filter(
-                      (channel) =>
-                        !closedCategories.includes(category.id) ||
-                        channel.unread
-                    )
-                    .map((channel: Channel) => (
-                      <ChannelLink channel={channel} key={channel.id} />
-                    ))}
-                </div>
+        <div className="flex-1 space-y-[21px] overflow-y-auto pt-3 font-medium text-gray-300 scrollbar scrollbar-thin scrollbar-thumb-gray-900">
+          {server.categories.map((category: Category) => (
+            <div key={category.id}>
+              {category.label && (
+                <button
+                  onClick={() => toggleCategory(category.id)}
+                  className="flex w-full items-center px-0.5 font-title text-xs uppercase tracking-wide hover:text-gray-100"
+                >
+                  <Icons.Arrow
+                    className={`${
+                      closedCategories.includes(category.id) ? '-rotate-90' : ''
+                    } mr-0.5 h-3 w-3 transition duration-200`}
+                  />
+                  {category.label}
+                </button>
+              )}
+
+              <div className="mt-[5px] space-y-0.5">
+                {category.channels
+                  .filter(
+                    (channel) =>
+                      !closedCategories.includes(category.id) || channel.unread
+                  )
+                  .map((channel: Channel) => (
+                    <ChannelLink channel={channel} key={channel.id} />
+                  ))}
               </div>
-            )
-          )}
+            </div>
+          ))}
         </div>
       </div>
-      <div className="flex flex-1 flex-col bg-gray-700">
-        <div className="flex h-12 items-center px-3 shadow-sm">General</div>
+
+      <div className="flex min-w-0 flex-1 flex-shrink flex-col bg-gray-700">
+        <div className="flex h-12 items-center px-3 shadow-sm">
+          <div className="flex items-center">
+            <Icons.Hashtag className="mx-2 h-6 w-6 font-semibold text-gray-400" />
+            <span className="mr-2 font-title text-white">{channel?.label}</span>
+          </div>
+
+          {channel?.description && (
+            <>
+              <div className="mx-2 h-6 w-px bg-white/[.06]"></div>
+              <div className="mx-2 truncate text-sm font-medium text-gray-200">
+                {channel?.description}
+              </div>
+            </>
+          )}
+
+          <div className="ml-auto flex items-center">
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.HashtagWithSpeechBubble className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.Bell className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.Pin className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.People className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.Inbox className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.QuestionCircle className="mx-2 h-6 w-6" />
+            </button>
+          </div>
+        </div>
+
         <div className="flex-1 space-y-4 overflow-y-auto p-3 scrollbar scrollbar-thin scrollbar-thumb-black scrollbar-track-gray-800">
           {[...Array(40)].map((_, index) => (
             <div key={index}>
