fd5b6cbc70cf86ca764bdca4e5d887ac4e687716
Adeonir Kohl <adeonir@gmail.com>
2022-02-09 23:23:25 -0300

    Update server header to match

--- src/pages/servers/[slug].js
+++ src/pages/servers/[slug].js
@@ -1,14 +1,21 @@
 import { useRouter } from 'next/router'
 
+import { CheckIcon, ChevronIcon, VerifiedIcon } from 'components/Icons'
+
 export default function Server() {
   const router = useRouter()
 
   return (
     <>
       <div className="flex w-60 flex-col bg-gray-800">
-        <div className="flex h-12 items-center border-b border-gray-800 px-3 font-title text-white shadow-md">
+        <button className="transition-200 flex h-12 items-center px-4 font-title text-[15px] text-white shadow-sm hover:bg-gray-550/[.16]">
+          <div className="relative mr-1 h-4 w-4">
+            <VerifiedIcon className="absolute h-4 w-4 text-gray-550" />
+            <CheckIcon className="absolute h-4 w-4 text-white" />
+          </div>
           {router.query.slug}
-        </div>
+          <ChevronIcon className="ml-auto h-[18px] w-[18px] opacity-80" />
+        </button>
         <div className="flex-1 space-y-3 overflow-y-auto p-3 font-medium text-gray-300 scrollbar scrollbar-thin scrollbar-thumb-gray-900">
           <p className="text-white">General</p>
           {[...Array(40)].map((_, index) => (
@@ -17,9 +24,7 @@ export default function Server() {
         </div>
       </div>
       <div className="flex flex-1 flex-col bg-gray-700">
-        <div className="flex h-12 items-center border-b border-gray-800 px-3 shadow-md">
-          General
-        </div>
+        <div className="flex h-12 items-center px-3 shadow-sm">General</div>
         <div className="flex-1 space-y-4 overflow-y-auto p-3 scrollbar scrollbar-thin scrollbar-thumb-black scrollbar-track-gray-800">
           {[...Array(40)].map((_, index) => (
             <div key={index}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   mode: 'jit',
   theme: {
+    boxShadow: {
+      sm: '0 1px 0 rgba(4,4,5,0.2),0 1.5px 0 rgba(6,6,7,0.05),0 2px 0 rgba(4,4,5,0.05)',
+      md: '0 4px 4px rgba(0,0,0,0.16)',
+      lg: '0 8px 16px rgba(0,0,0,0.24)',
+    },
     extend: {
       fontFamily: {
         sans: ['Whitney', ...defaultTheme.fontFamily.sans],
@@ -18,6 +23,7 @@ module.exports = {
           300: '#8e9297',
           400: '#72767d',
           500: '#5c6067',
+          550: '#4f545c',
           600: '#464950',
           700: '#36393f',
           800: '#2f3136',
